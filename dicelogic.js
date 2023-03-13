f3ff09c65111a59c6182cd55914d04235dd38f47
albertoar8 <albertoar8@gmail.com>
2023-03-13 18:07:14 +1100

    Push the button and it will print in the console

--- dicelogic.js
+++ dicelogic.js
@@ -7,36 +7,13 @@ let rollCount = {};
 rollCount["dice-0"] = 0;
 let gotTotal = false;
 
-function diceHTML(diceID) {
-    return `
-    <div id="dice-0" class="col">
-    <div id="dice" align="center" class="border border-5 rounded-2"">
-    <h2 id="diceTitle">D 6</h2>
-    <button class="btn-close btn-close-white" style="margin:0.2rem; position:absolute; top:0; right:0;" onclick="removeDice('${diceID}')"></button><br>
-    
-    <label style="display: block" for="sidesDropdown">Sides:</label>
-    
-    <select id="sidesDropdown" onchange="dropDownChanged('${diceID}')">
-        <option>2</option>
-        <option>4</option>
-        <option selected>6</option>
-        <option>8</option>
-        <option>10</option>
-        <option>12</option>
-        <option>20</option>
-        <option>Custom</option>
-    </select>
-    <input id="customSides" onchange="customSidesChanged('${diceID}')" type="number" style="width:40%; display: none" value="50"></input>
-    
-    <h5>Result:</h5>
-    <div id="resultBorder" align="center" class="justif border border-5 rounded-4 d-flex justify-content-center align-items-center" style="width:6.5rem; height:6.5rem;">
-        <h5 id="result">0</h5>
-    </div>
-    <button id="btnRoll" class="btn btn-light" style="margin: 0.2rem" onclick="roll('${diceID}'); rollAnimStart('${diceID}'); gotTotal=false; disableAllButtons();">Roll</button>
-    </div><!--Dice div-->
-    </div><!--Dice col-->`
+printResult();
+function printResult(){
+    let dice = Math.random()*6; //dice of 6 sides
+    dice = Math.round(dice);
+    console.log("Hi This is the dice result:  "+dice);
 }
-
+/*
 function disableAllButtons(){
     document.getElementById("btnRollAll").disabled = true;
     document.getElementById("btnAddDice").disabled = true;
@@ -161,7 +138,6 @@ function dropDownChanged(_id){
         diceCustomInput.style.display = "none";
     }
 }
-
 // Validate the sides selection
 function customSidesChanged(_id){
     let diceCol = document.querySelector(`#${_id}`);
@@ -178,3 +154,4 @@ function customSidesChanged(_id){
     diceCol.querySelector("#customSides").value = sides;
     diceTitle.innerHTML = `D ${sides}`;
 }
+*/
